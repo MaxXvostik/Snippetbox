65b9c74d1f534a6ea44c73b39440b1d3e0dbc502
MaxXvostik <xvost-m@mail.ru>
2022-11-02 22:20:23 +0300

    lesson_3

--- main.go
+++ main.go
@@ -7,7 +7,26 @@ import (
 
 // Создается функция-обработчик "home"
 func home(w http.ResponseWriter, r *http.Request) {
+
+	// Проверяется, если текущий путь URL запроса точно совпадает с шаблоном "/". Если нет, вызывается
+	// функция http.NotFound() для возвращения клиенту ошибки 404.
+	// Важно, чтобы мы завершили работу обработчика через return.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Привет из Snippetbox"))
+}
+
+// Обработчик для отображения содержимого заметки.
+func showSnippert(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отоброжение заметки..."))
+}
+
+// Обработчик для создания новой заметки.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Форма для создания новой заметки..."))
 }
 
 func main() {
@@ -15,6 +34,8 @@ func main() {
 	// Используется функция http.NewServeMux() для инициализации нового рутера
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippert", showSnippert)
+	mux.HandleFunc("/snippert/create", createSnippet)
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 
