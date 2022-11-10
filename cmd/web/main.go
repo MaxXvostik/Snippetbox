3797a542bc523da9c2a4aa51ca29fdfd4d2e0502
MaxXvostik <xvost-m@mail.ru>
2022-11-10 20:39:47 +0300

    lesson_10

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+	// Мы вызываем функцию flag.Parse() для извлечения флага из командной строки.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Запуск сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Запуск сервера на %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
